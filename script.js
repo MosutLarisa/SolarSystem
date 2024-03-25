2efc9a902206572984fd77127215283bb62bca51
Larisa Moșuț <larisa.mosut@emanuel.ro>
2024-03-25 17:42:58 +0200

    am adaugat 5 ellipse pentru orbita

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ function setup() {
 }
 
 function draw() {
+    stroke("black");
+    fill("white");
+    ellipse(660, 350, 1300, 600);
+    ellipse(600, 350, 1100, 500);
+    ellipse(610, 350, 900, 400);
+    ellipse(650, 350, 700, 300);
+    ellipse(650, 350, 450, 250);
 
 }
